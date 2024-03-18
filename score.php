47666d0781dfff97b3a2b10e8fc9861b0ebd11f6
Krisskyy <kriss060428@gmail.com>
2024-03-18 14:46:33 +0100

    PHP improvement and @Media

--- score.php
+++ score.php
@@ -3,10 +3,25 @@ $conn = new mysqli('localhost', 'root', '', 'surveydb');
 $conn->set_charset('utf8');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $score = $_POST['score'];
-    $conn->query("INSERT INTO scores VALUES ('', '$score', 1)");
-    $conn->close();
-    header('Location: quest_survey.html');
-    exit;
+    if (isset($_POST['score'])) {
+        if (filter_var($_POST['score'], FILTER_VALIDATE_INT) !== false) {
+            $stmt = $conn->prepare("INSERT INTO scores (score_count, user_id) VALUES (?, ?)");
+            $user_id = 1;
+            $stmt->bind_param("ii", $_POST['score'], $user_id);
+
+            if ($stmt->execute()) {
+                $stmt->close();
+                $conn->close();
+                header('Location: quest_survey.html');
+                exit;
+            } else {
+                echo "Błąd: " . $conn->error;
+            }
+        } else {
+            echo "Punktacja musi być liczbą całkowitą.";
+        }
+    } else {
+        echo "Brak przesłanej punktacji.";
+    }
 }
 ?>
