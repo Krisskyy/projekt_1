98694fb98fd50347dea64c4d38b577928e95461d
Krisskyy <kriss060428@gmail.com>
2024-03-22 14:30:11 +0100

    Media and PHP bug fixes

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,6 +1,7 @@
 <?php
 class SurveyProcessor {
     private $conn;
+    protected $positions = 5;
 
     function __construct() {
         $this->conn = new mysqli('localhost', 'root', '', 'surveydb');
@@ -204,8 +205,9 @@ class SurveyProcessor {
                 
             if(isset($_POST['page'])) {
                 $page = intval($_POST['page']);
-                $itemsPerPage = intval($_POST['positions']);
-                
+                $positions = isset($_POST['positions']) ? $_POST['positions'] : 5;
+                $itemsPerPage = intval($positions);
+
                 $items = $itemsPerPage * $page;
                 if ($items > $total_pages) {
                     $items = $total_pages;
@@ -232,7 +234,244 @@ class SurveyProcessor {
             return $itemsTwo;
         }
     }
+
+    class SurveyProcessorWinners {
+        private $conn;
+        protected $positionsWinners = 5;
+    
+        function __construct() {
+            $this->conn = new mysqli('localhost', 'root', '', 'surveydb');
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
+            $this->conn->set_charset('utf8');
+        }
+    
+        function __destruct() {
+            $this->conn->close();
+        }
+    
+        public function showData() {
+            $searchWinners = '';
+            if(isset($_POST['searchWinners'])){
+                $searchWinners = $_POST['searchWinners'];
+            }
+    
+            if(!$searchWinners) {
+                $positionsWinners = $this->getPositions();
+    
+                $between = $this->getBetweenValues($positionsWinners);
+    
+                $q = "SELECT
+                        users.userID,
+                        CONCAT(users.name, ' ', users.surname) AS full_name,
+                        DATE_FORMAT(scores.date, '%d.%m.%Y, %H:%i') AS date,
+                        scores.score,
+                        (SELECT COUNT(*) + 1
+                            FROM scores AS s
+                            WHERE s.score > scores.score
+                            OR (s.score = scores.score AND s.date < scores.date)) AS rank,
+                        scores.win,
+                        CONCAT(address.street, ' ', address.housenumber,
+                                IF(address.apartmentnumber <> '', CONCAT('/', address.apartmentnumber), ''),
+                                ' ', address.zipcode, ' ', address.location) AS full_address,
+                        surveynps.score AS nps_score,
+                        surveynps.comment AS nps_comment
+                        FROM
+                            users
+                        LEFT JOIN
+                            scores ON users.userID = scores.userID
+                        LEFT JOIN
+                            address ON users.addressID = address.addressID
+                        LEFT JOIN
+                            surveynps ON scores.surveyID = surveynps.surveyID WHERE scores.win = '1' AND users.userid BETWEEN ? AND ? LIMIT ?";
+    
+                $stmt = $this->conn->prepare($q);
+                $stmt->bind_param("iii", $between['betweenOne'], $between['betweenTwo'], $positionsWinners);
+            } else {
+                $positionsWinners = $this->getPositions();
+                $q = "SELECT
+                        users.userID,
+                        CONCAT(users.name, ' ', users.surname) AS full_name,
+                        DATE_FORMAT(scores.date, '%d.%m.%Y, %H:%i') AS date,
+                        scores.score,
+                        (SELECT COUNT(*) + 1
+                            FROM scores AS s
+                            WHERE s.score > scores.score
+                            OR (s.score = scores.score AND s.date < scores.date)) AS rank,
+                        scores.win,
+                        CONCAT(address.street, ' ', address.housenumber,
+                                IF(address.apartmentnumber <> '', CONCAT('/', address.apartmentnumber), ''),
+                                ' ', address.zipcode, ' ', address.location) AS full_address,
+                        surveynps.score AS nps_score,
+                        surveynps.comment AS nps_comment
+                        FROM
+                            users
+                        LEFT JOIN
+                            scores ON users.userID = scores.userID
+                        LEFT JOIN
+                            address ON users.addressID = address.addressID
+                        LEFT JOIN
+                            surveynps ON scores.surveyID = surveynps.surveyID 
+                        WHERE scores.win = '1' AND users.name LIKE ? OR users.surname LIKE ? AND scores.win = '1' OR concat(users.name, ' ', users.surname) LIKE ? AND scores.win = '1' LIMIT ?";
+    
+                $stmt = $this->conn->prepare($q);
+                $searchWinners = "%$searchWinners%";
+                $stmt->bind_param("sssi", $searchWinners, $searchWinners, $searchWinners, $positionsWinners);
+            }
+    
+            $stmt->execute();
+            $stmt->bind_result($userID, $full_name, $date, $score, $rank, $win, $full_address, $nps_score, $nps_comment);
+    
+            while($stmt->fetch()) {
+                echo "<tr>";
+                echo "<td><button class=\"wrapped\" type=\"button\"></button></td>";
+                echo "<td>" . htmlspecialchars($userID) . "</td>";
+                echo "<td>" . htmlspecialchars($full_name) . "</td>";
+                echo "<td>" . htmlspecialchars($date) . "</td>";
+                echo "<td>" . htmlspecialchars($score) . "</td>";
+                echo "<td>" . htmlspecialchars($rank) . "</td>";
+                echo "<td>";
+                echo ($win == 1) ? "<div class=\"stm-true\"><img src=\"./dashboardMedia/true.svg\" alt=\"true\">TAK</div>" : "<div class=\"stm-false\"><img src=\"./dashboardMedia/false.svg\" alt=\"false\">NIE</div>";
+                echo "</td>";
+                echo ($full_address != NULL) ? "<td>" . htmlspecialchars($full_address) . "</td>" : "<td> - </td>";
+                echo "</tr>";
+                echo "<tr class=\"none\">";
+                echo "<td style=\"height: 120px;\" colspan=\"8\">";
+                echo "<div class=\"info-container\">";
+                echo "<div class=\"info-header\">";
+                echo "<h2>Czas rozwiązania <br> konkursu <p>00:06:12</p></h2>";
+                echo "<h2>Data przesłania <br> danych teleadresowych <p> ". htmlspecialchars($date) ."</p></h2>";
+                echo ($nps_score != NULL) ? "<h2>Ocena NPS <p> ". htmlspecialchars($nps_score) ."</p></h2>" : "<h2>Ocena NPS <p> - </p></h2>";
+                echo ($nps_comment != NULL) ? "<h2>Komentarz <p class=\"comment\">". htmlspecialchars($nps_comment) ."</p></h2>" : "<h2>Komentarz <p class=\"comment\"> - </p></h2>";
+                echo "</div>";
+                echo "</div>";
+                echo "</td>";
+                echo "</tr>";
+            }
+    
+            $stmt->close();
+        }
+    
+        private function getPositions() {
+            $items = 0;
+            $q = "SELECT COUNT(scores.scoreID) AS total FROM scores;";
+            $result = $this->conn->query($q);
+            $row = $result->fetch_assoc();
+            $total_pages = $row['total'];
+    
+            if(isset($_POST['positionsWinners'])) {
+                if ($_POST['positionsWinners'] > $total_pages) {
+                    $items = $total_pages;
+                } else {
+                    $items = $_POST['positionsWinners'];
+                }
+    
+            } else {
+                $items = 5;
+            }
+            return $items;
+        }
+    
+        private function getBetweenValues($positionsWinners) {
+            if ($positionsWinners == 5) {
+                $betweenOne = 1;
+                $betweenTwo = 5;
+                if (isset($_POST['page'])) {
+                    $page = $_POST['page'];
+                    $betweenOne = 5 * ($page - 1) + 1;
+                    $betweenTwo = 5 * $page;
+                }
+            } elseif ($positionsWinners == 10) {
+                $betweenOne = 1;
+                $betweenTwo = 10;
+                if (isset($_POST['page'])) {
+                    $page = $_POST['page'];
+                    $betweenOne = 10 * ($page - 1) + 1;
+                    $betweenTwo = 10 * $page;
+                    }
+                    } elseif ($positionsWinners == 15) {
+                        $betweenOne = 1;
+                        $betweenTwo = 15;
+                    if (isset($_POST['page'])) {
+                        $page = $_POST['page'];
+                        $betweenOne = 15 * ($page - 1) + 1;
+                        $betweenTwo = 15 * $page;
+                    }
+                    }
+                return array("betweenOne" => $betweenOne, "betweenTwo" => $betweenTwo);
+            }
+                    
+            public function pages() {
+                $q = "SELECT COUNT(scores.scoreID) AS total FROM scores";
+                $stmt = $this->conn->prepare($q);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $row = $result->fetch_assoc();
+                $total_pages = intval($row['total']);
+                $stmt->close();
+                return $total_pages;
+            }
+    
+    
+            public function getPageNumber() {
+                $conn = new mysqli('localhost', 'root', '', 'surveydb');
+                    
+                $q = "SELECT COUNT(scores.scoreID) AS total FROM scores;";
+                $result = $conn->query($q);
+                $row = $result->fetch_assoc();
+                $total_pages = $row['total'];
+                    
+                $items = 0;
+                if(isset($_POST['positionsWinners'])) {
+                    if ($_POST['positionsWinners'] > $total_pages) {
+                        $items = $total_pages;
+                    } else {
+                        if($_POST['positionsWinners'] == 5) {
+                            $items = 5;
+                        } else if($_POST['positionsWinners'] == 10) {
+                            $items = 10;
+                        } else if($_POST['positionsWinners'] == 15) {
+                            $items = 15;
+                        }
+                    }
+                } else {
+                    $items = 5;
+                }
+                    
+                if(isset($_POST['page'])) {
+                    $page = intval($_POST['page']);
+                    $positionsWinners = isset($_POST['positionsWinners']) ? $_POST['positionsWinners'] : 5;
+                    $itemsPerPage = intval($positionsWinners);
+                    
+                    $items = $itemsPerPage * $page;
+                    if ($items > $total_pages) {
+                        $items = $total_pages;
+                    }
+                }
+                    
+                    
+                return $items;
+            }
+    
+            public  function getSecondPageNumber() {
+                $itemsTwo = 1;
+                $positionsWinners = $this->getPageNumber();
+                    
+                if(isset($_POST['positionsWinners'])) {
+                    if($_POST['positionsWinners'] == 5) {
+                        $itemsTwo = $positionsWinners - 4; 
+                    } else if($_POST['positionsWinners'] == 10) {
+                        $itemsTwo = $positionsWinners - 9; 
+                    } else if($_POST['positionsWinners'] == 15) {
+                        $itemsTwo = $positionsWinners - 14; 
+                    }
+                }
+                return $itemsTwo;
+            }
+        }
     $surveyProcessor = new SurveyProcessor();
+    $surveyProcessorWinners = new SurveyProcessorWinners();
 ?>
 
 
@@ -289,8 +528,8 @@ class SurveyProcessor {
 
                 <section class="content all">
                     <div class="content-header">
-                        <button type="button" class="activated-button">Lista uczestników</button>
-                        <button type="button" class="disabled-button" onclick="switchInformations()">Lista zwycięzców</button>
+                        <button type="button" class="activated-button" onclick="switchInformations('all')">Lista uczestników</button>
+                        <button type="button" class="disabled-button" id="winnersButton" onclick="switchInformations('winners')">Lista zwycięzców</button>
                     </div>
                     <div class="content-search">
                         <h3>Wyszukaj użytkownika</h3>
@@ -348,13 +587,13 @@ class SurveyProcessor {
             <form action="dashboard.php" method="post" id="pagesWinners">
                 <section class="content winners none">
                     <div class="content-header">
-                        <button type="button" class="disabled-button" onclick="switchInformations()">Lista uczestników</button>
-                        <button type="button" class="activated-button">Lista zwycięzców</button>
+                        <button type="button" class="disabled-button" onclick="switchInformations('all')">Lista uczestników</button>
+                        <button type="button" class="activated-button" id="winnersButton" onclick="switchInformations('winners')">Lista zwycięzców</button>
                     </div>
                     <div class="content-search">
                         <h3>Wyszukaj użytkownika</h3>
                         <div class="input-button">
-                                <input type="text" name="search" id="search">
+                                <input type="text" name="searchWinners" id="searchWinners">
                                 <button type="submit" class="search-button"></button>
                         </div>
                     </div>
@@ -373,19 +612,19 @@ class SurveyProcessor {
                                 </tr>
                             </thead>
                             <tbody>
-                            <?php //$surveyProcessor->showData(); ?>
+                            <?php $surveyProcessorWinners->showData(); ?>
                             </tbody>
                         </table>
                     </div>
                     <div class="content-footer">
                         <div class="footer-left">
-                            <h5>Pozycje od <?php echo $surveyProcessor->getSecondPageNumber(); ?> do <?php echo $surveyProcessor->getPageNumber(); ?> z <?php echo $surveyProcessor->pages(); ?> łącznie</h5>
+                            <h5>Pozycje od <?php echo $surveyProcessorWinners->getSecondPageNumber(); ?> do <?php echo $surveyProcessorWinners->getPageNumber(); ?> z <?php echo $surveyProcessorWinners->pages(); ?> łącznie</h5>
                             <h6>
                                 Pokaż 
-                                    <select id="positions" name="positions" onchange="submitForm()">
-                                    <option value="5" <?php if(isset($_POST['positions']) && $_POST['positions'] == "5") echo "selected"; ?>>5</option>
-                                    <option value="10" <?php if(isset($_POST['positions']) && $_POST['positions'] == "10") echo "selected"; ?>>10</option>
-                                    <option value="15" <?php if(isset($_POST['positions']) && $_POST['positions'] == "15") echo "selected"; ?>>15</option>
+                                    <select id="positionsWinners" name="positionsWinners" onchange="submitFormWinners()">
+                                    <option value="5" <?php if(isset($_POST['positionsWinners']) && $_POST['positionsWinners'] == "5") echo "selected"; ?>>5</option>
+                                    <option value="10" <?php if(isset($_POST['positionsWinners']) && $_POST['positionsWinners'] == "10") echo "selected"; ?>>10</option>
+                                    <option value="15" <?php if(isset($_POST['positionsWinners']) && $_POST['positionsWinners'] == "15") echo "selected"; ?>>15</option>
                                     </select>
                                 pozycji
                             </h6>
--- dashboard/dashboard_script.js
+++ dashboard/dashboard_script.js
@@ -11,10 +11,34 @@ function submitForm() {
     document.getElementById("pages").submit();
 }
 
-function switchInformations(){
+function submitFormWinners() {
+    document.getElementById("pagesWinners").submit();
+}
+
+window.onload = function() {
+    let currentPage = localStorage.getItem("currentPage");
+
+    if (currentPage === "winners") {
+        switchInformations('winners');
+    }
+};
+
+function switchInformations(section) {
     let all = document.querySelector('.all');
     let winners = document.querySelector('.winners');
+    let winnersButton = document.getElementById('winnersButton');
 
-    all.classList.toggle('none');
-    winners.classList.toggle('none');
+    if (section === 'all') {
+        all.classList.remove('none');
+        winners.classList.add('none');
+        winnersButton.classList.remove('activated-button');
+        winnersButton.classList.add('disabled-button');
+    } else if (section === 'winners') {
+        all.classList.add('none');
+        winners.classList.remove('none');
+        winnersButton.classList.remove('disabled-button');
+        winnersButton.classList.add('activated-button');
+    }
+
+    localStorage.setItem("currentPage", section);
 }
